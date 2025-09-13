63a48dcd2c3aa20cff92ffcabffe2b1cc577ba4e
monoton <andrey.tkachev.k@gmail.com>
2025-09-13 12:39:53 +0200

    fixed 2nd task solution

--- school/homework/10_09_25.py
+++ school/homework/10_09_25.py
@@ -7,7 +7,7 @@
 
 def task_2():
     a, b = map(int, input().split())
-    print(3 * pow((a + b), 3) + 275 * pow(b, 3) - 127 * a - 41)
+    print(3 * pow((a + b), 3) + 275 * pow(b, 2) - 127 * a - 41)
     return 0
 
 
@@ -27,10 +27,10 @@ def task_4():
 
 
 if __name__ == "__main__":
-    print("task num 2")
-    task_2()
-    print("task num 3")
-    task_3()
+    # print("task num 2")
+    # task_2()
+    # print("task num 3")
+    # task_3()
     print("task num 4")
     task_4()
     input("press enter to exit")
